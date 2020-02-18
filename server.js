c6c16b38f6297b690c7bca5a1faf8a26aa176168
KHELOUAT Abdelmalek <ab.khelouat@gmail.com>
2020-02-18 22:36:57 +0100

    add body parser to send data

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var port = 3000;
 var usersMocks = require('./mocks');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 
 app.get('/', (req, res) => {
     res.send('le serveur fonctionne bien')
@@ -9,6 +12,10 @@ app.get('/', (req, res) => {
 
 app.get('/api/getData', (req, res) => {
     res.status(200).json(usersMocks)
+});
+
+app.post('/api/sendData', (req, res) => {
+    res.status(201).send('Les données ont bien étés enregistrés !')
 });
 
 app.listen(port, console.log('listen on' + port));
