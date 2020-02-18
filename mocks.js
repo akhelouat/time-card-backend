42bcd34b7a663a268e5bc058278fe2792b09e1dd
KHELOUAT Abdelmalek <ab.khelouat@gmail.com>
2020-02-18 22:23:22 +0100

    create usersMock in mocks.js and export it

---
+++ mocks.js
@@ -0,0 +1,20 @@
+var usersMock = {
+    "1":{
+        "name": 'philippe',
+        "password": '1234',
+        "promo": "1"
+        },
+    "2":{
+        "name": 'felix',
+        "password": '1234',
+        "promo": "1"
+        },
+    "3":{
+        "name": 'stephane',
+        "password": '1234',
+        "promo": "1"
+        }
+        
+}
+
+module.exports = usersMock;
