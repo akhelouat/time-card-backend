a34a29d56461c06943c81ba9bfc986e4a7fec3ea
KHELOUAT Abdelmalek <ab.khelouat@gmail.com>
2020-02-22 18:55:52 +0100

    add delete method in promo controller

--- controllers/promo.js
+++ controllers/promo.js
@@ -22,17 +22,17 @@ end: req.body.end
 exports.updatePromo = (req, res, next) => {
   if (req.body._id) {
      if(req.body.name) {
-      Member.updateMany({_id : req.body._id }, { $set: { name: req.body.name } })
+      Mypromo.updateMany({_id : req.body._id }, { $set: { name: req.body.name } })
       .then(Myinfo => res.status(200).send('le nom de la promo a bien été changé en ' + req.body.username + ' || '))
       .catch(Myinfo => res.status(400).send('problème : aucune modifications de faite'));
      }
      if(req.body.start) {
-      Member.updateMany({_id : req.body._id }, { $set: { start: req.body.start } })
+      Mypromo.updateMany({_id : req.body._id }, { $set: { start: req.body.start } })
       .then(Myinfo => res.status(200).send('la date du début de la promo a bien été changée en '+ req.body.start +'  || '))
       .catch(Myinfo => res.status(400).send('problème : aucune modifications de faite'));
      }
      if(req.body.end) {
-      Member.updateMany({_id : req.body._id }, { $set: { end: req.body.end } })
+      Mypromo.updateMany({_id : req.body._id }, { $set: { end: req.body.end } })
       .then(Myinfo => res.status(200).send('la date de fin de la promo a bien été changée en ' + req.body.end + ' || '))
       .catch(Myinfo => res.status(400).send('problème : aucune modifications de faite'));
      }
@@ -43,3 +43,19 @@ exports.updatePromo = (req, res, next) => {
 
      }
   };
+
+  exports.deletePromo = (req, res, next) => {
+    if(req.body._id)
+    {
+      Mypromo.deleteOne({_id : req.body._id })
+         .then(() => res.status(200).send('la suppression a bien été effectuée'))
+         .catch(() => res.status(400).send('l\'id fournit ne match avec aucune promo, veuillez renvoyer une autre requete'))
+    }
+    else
+    {
+        res.status(400).send('problème : il faut préciser l\'id de la promo ')
+    }       
+
+
+
+};
