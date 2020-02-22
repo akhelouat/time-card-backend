5acbc07a4ba55af5b9f48bf049ded78f78c9f99a
KHELOUAT Abdelmalek <ab.khelouat@gmail.com>
2020-02-22 15:57:51 +0100

    delete useless file /controllers/members_controller.js

--- router.js
+++ router.js
@@ -12,41 +12,22 @@ mongoose.connect('mongodb://localhost:27017/timecard',
   useUnifiedTopology: true })
 .then(() => console.log('Connexion à MongoDB réussie !'))
 .catch(() => console.log('Connexion à MongoDB échouée !'));
-
 router.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     next();
   });
 
-  router.post('/add/promo', promoController.addPromo);
-
-
-
-  router.get('/get/promo', promoController.getPromo)
+router.post('/add/promo', promoController.addPromo);
+router.get('/get/promo', promoController.getPromo)
  
- 
- 
-  router.post('/add/info', infoController.addInfo);
-
-
-
-  router.get('/get/info', infoController.getInfo)
+router.post('/add/info', infoController.addInfo);
+router.get('/get/info', infoController.getInfo)
 
   
 router.post('/add/member', memberController.addMember);
-
-
-
 router.get('/get/member', memberController.getMember)
   
-
 router.post('/add/date', dateController.addDate);
-
-
-
 router.get('/get/date', dateController.getDate)
 
-
-    
-    
 module.exports = router;
