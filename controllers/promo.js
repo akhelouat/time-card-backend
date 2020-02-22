19b848b6be7fe553916afb5f7860539e11fd3d21
KHELOUAT Abdelmalek <ab.khelouat@gmail.com>
2020-02-22 18:53:50 +0100

    create update method in promo controller

--- controllers/promo.js
+++ controllers/promo.js
@@ -18,3 +18,28 @@ end: req.body.end
       .catch(Mypromo => res.status(400).json({ Mypromo }));
     
 };
+
+exports.updatePromo = (req, res, next) => {
+  if (req.body._id) {
+     if(req.body.name) {
+      Member.updateMany({_id : req.body._id }, { $set: { name: req.body.name } })
+      .then(Myinfo => res.status(200).send('le nom de la promo a bien été changé en ' + req.body.username + ' || '))
+      .catch(Myinfo => res.status(400).send('problème : aucune modifications de faite'));
+     }
+     if(req.body.start) {
+      Member.updateMany({_id : req.body._id }, { $set: { start: req.body.start } })
+      .then(Myinfo => res.status(200).send('la date du début de la promo a bien été changée en '+ req.body.start +'  || '))
+      .catch(Myinfo => res.status(400).send('problème : aucune modifications de faite'));
+     }
+     if(req.body.end) {
+      Member.updateMany({_id : req.body._id }, { $set: { end: req.body.end } })
+      .then(Myinfo => res.status(200).send('la date de fin de la promo a bien été changée en ' + req.body.end + ' || '))
+      .catch(Myinfo => res.status(400).send('problème : aucune modifications de faite'));
+     }
+  }
+  else
+     {
+      res.status(400).send('problème : il faut préciser l\'id de l\'étudiant ')
+
+     }
+  };
