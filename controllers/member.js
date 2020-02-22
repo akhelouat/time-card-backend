5173e4c839fd60b41217c6992d67d485fba3cd10
KHELOUAT Abdelmalek <ab.khelouat@gmail.com>
2020-02-22 18:23:56 +0100

    add delete method in member controller

--- controllers/member.js
+++ controllers/member.js
@@ -24,17 +24,17 @@ exports.getMember = (req, res, next) => {
 exports.updateMember = (req, res, next) => {
     if (req.body._id) {
        if(req.body.username) {
-        Myinfo.updateMany({_id : req.body._id }, { $set: { username: req.body.username } })
+        Member.updateMany({_id : req.body._id }, { $set: { username: req.body.username } })
         .then(Myinfo => res.status(200).send('le username a bien été changé en ' + req.body.username + ' || '))
         .catch(Myinfo => res.status(400).send('problème : aucune modifications de faite'));
        }
        if(req.body.password) {
-        Myinfo.updateMany({_id : req.body._id }, { $set: { password: req.body.password } })
+        Member.updateMany({_id : req.body._id }, { $set: { password: req.body.password } })
         .then(Myinfo => res.status(200).send('le mot de pass a bien été changé  || '))
         .catch(Myinfo => res.status(400).send('problème : aucune modifications de faite'));
        }
        if(req.body.isAdmin) {
-        Myinfo.updateMany({_id : req.body._id }, { $set: { isAdmin: req.body.isAdmin } })
+        Member.updateMany({_id : req.body._id }, { $set: { isAdmin: req.body.isAdmin } })
         .then(Myinfo => res.status(200).send('le status administrateur a bien été changé en ' + req.body.isAdmin + ' || '))
         .catch(Myinfo => res.status(400).send('problème : aucune modifications de faite'));
        }
@@ -44,6 +44,18 @@ exports.updateMember = (req, res, next) => {
         res.status(400).send('problème : il faut préciser l\'id de l\'étudiant ')
 
        }
+
+       exports.deleteInfo = (req, res, next) => {
+        if(req.body._id)
+        {
+             Member.deleteOne({_id : req.body._id })
+             .then(() => res.status(200).send('la suppression a bien été effectuée'))
+             .catch(() => res.status(400).send('l\'id fournit ne match avec aucun étudiant, veuillez renvoyer une autre requete'))
+        }
+        else
+        {
+            res.status(400).send('problème : il faut préciser l\'id de l\'étudiant ')
+        }       
 
     
 
