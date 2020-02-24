65aeb4ea13195bc0487d5e171828c6abae28a0f9
stephane <stephane.vieira75@outlook.fr>
2020-02-25 00:49:28 +0100

    switch for request update

--- controllers/info.js
+++ controllers/info.js
@@ -28,13 +28,117 @@ exports.getInfo = (req, res, next) => {
         .catch(Myinfo => res.status(400).json({
             Myinfo
         }));
-
 };
 
 
 
 exports.updateInfo = (req, res, next) => {
-    if (req.body.nom) {
+    
+    switch (req) {
+        // Nom
+        case req.body.nom:
+            Myinfo.updateMany({
+                _id: req.body._id
+            },
+            {
+                $set: { nom : req.body.nom }
+            })
+            .then(Myinfo => res.status(200).send('le nom a bien été changé en ' + req.body.nom + ' || '))
+            .catch(Myinfo => res.status(400).send('problème : aucune modifications de faite'));
+            break;
+        // Prenom
+        case req.body.prenom:
+            Myinfo.updateMany({
+                _id: req.body._id
+            },
+            {
+                $set: { prenom: req.body.prenom }
+            })
+            .then(Myinfo => res.status(200).send('le nom a bien été changé en ' + req.body.prenom + ' || '))
+            .catch(Myinfo => res.status(400).send('problème : aucune modifications de faite'));
+            break;
+        // Email
+        case req.body.email:
+            Myinfo.updateMany({
+                _id: req.body._id
+            },
+            {
+                $set: { email: req.body.email }
+            })
+            .then(Myinfo => res.status(200).send('l\'email a bien été changé en ' + req.body.email + ' || '))
+            .catch(Myinfo => res.status(400).send('problème : aucune modifications de faite'));
+            break;
+        // Address
+        case req.body.addr:
+            Myinfo.updateMany({
+                _id: req.body._id
+            },
+            {
+                $set: { addr: req.body.addr }
+            })
+            .then(Myinfo => res.status(200).send('l\'adresse a bien été changé en ' + req.body.addr + ' || '))
+            .catch(Myinfo => res.status(400).send('problème : aucune modifications de faite'));
+            break;
+        // CP Address
+        case req.body.cp:
+            Myinfo.updateMany({
+                _id: req.body._id
+            },
+            {
+                $set: { cp: req.body.cp }
+            })
+            .then(Myinfo => res.status(200).send('l\'adresse postale a bien été changée en ' + req.body.cp + ' || '))
+            .catch(Myinfo => res.status(400).send('problème : aucune modifications de faite'));
+            break;
+        // Phone
+        case req.body.phone:
+            Myinfo.updateMany({
+                _id: req.body._id
+            },
+            {
+                $set: { phone: req.body.phone }
+            })
+            .then(Myinfo => res.status(200).send('le numéro de téléphone a bien été changé en ' + req.body.phone + ' || '))
+            .catch(Myinfo => res.status(400).send('problème : aucune modifications de faite'));
+            break;
+        // Sign
+        case req.body.sign:
+            Myinfo.updateMany({
+                _id: req.body._id
+            },
+            {
+                $set: { sign: req.body.sign }
+            })
+            .then(Myinfo => res.status(200).send('la signature a bien été changée en ' + req.body.email + ' || '))
+            .catch(Myinfo => res.status(400).send('problème : aucune modifications de faite'));
+            break;
+        // Picture
+        case req.body.picture:
+            Myinfo.updateMany({
+                _id: req.body._id
+            },
+            {
+                $set: { picture: req.body.picture }
+            })
+            .then(Myinfo => res.status(200).send('la photo a bien été changée en ' + req.body.addr + ' || '))
+            .catch(Myinfo => res.status(400).send('problème : aucune modifications de faite'));
+            break;
+        // ID Pole emploi
+        case req.body.idPole:
+            Myinfo.updateMany({
+                _id: req.body._id
+            },
+            {
+                $set: { idPole: req.body.idPole }
+            })
+            .then(Myinfo => res.status(200).send('le numéro pole emploi a bien été changé en ' + req.body.addr + ' || '))
+            .catch(Myinfo => res.status(400).send('problème : aucune modifications de faite'));
+            break;
+        default:
+            console.log('Bad request lol');
+    };
+
+    /*if (req.body.nom) {
         Myinfo.updateMany({
                 _id: req.body._id
             }, {
@@ -132,7 +236,7 @@ exports.updateInfo = (req, res, next) => {
             })
             .then(Myinfo => res.status(200).send('le numéro pole emploi a bien été changé en ' + req.body.addr + ' || '))
             .catch(Myinfo => res.status(400).send('problème : aucune modifications de faite'));
-    }
+    }*/
 };
 
 exports.deleteInfo = (req, res, next) => {
