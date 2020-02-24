41c7a353e1a97fb8d17f6ccfe730edf0336720fc
stephane <stephane.vieira75@outlook.fr>
2020-02-24 23:05:14 +0100

    remove ; in router.js

--- router.js
+++ router.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require('express')
 const bodyParser = require('body-parser')
 const router = express()
-const mongoose = require('mongoose');
-router.use(bodyParser.json());
+const mongoose = require('mongoose')
+router.use(bodyParser.json())
 const dateController = require('./controllers/date')
 const memberController = require('./controllers/member')
 const promoController = require('./controllers/promo')
@@ -14,33 +14,33 @@ mongoose.connect('mongodb+srv://abdel:3wRrhcbY96MCdRD@cluster0-axmws.gcp.mongodb
     useUnifiedTopology: true
   })
   .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+  .catch(() => console.log('Connexion à MongoDB échouée !'))
 router.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
+  res.header("Access-Control-Allow-Origin", "*")
+  next()
+})
 
 // Promo
-router.post('/add/promo', promoController.addPromo);
+router.post('/add/promo', promoController.addPromo)
 router.get('/get/promo', promoController.getPromo)
-router.put('/update/promo', promoController.updatePromo);
+router.put('/update/promo', promoController.updatePromo)
 router.delete('/delete/promo', promoController.deletePromo)
 
 // Info
-router.post('/add/info', infoController.addInfo);
+router.post('/add/info', infoController.addInfo)
 router.get('/get/info', infoController.getInfo)
 router.put('/update/info', infoController.updateInfo)
 router.delete('/delete/info', infoController.deleteInfo)
 
 // Member
-router.post('/add/member', memberController.addMember);
+router.post('/add/member', memberController.addMember)
 router.get('/get/member', memberController.getMember)
 router.put('/update/member', memberController.updateMember)
 router.delete('/delete/member', memberController.deleteMember)
 
 // Date
-router.post('/add/date', dateController.addDate);
+router.post('/add/date', dateController.addDate)
 router.get('/get/date', dateController.getDate)
 router.put('/update/date', dateController.updateDate)
 
-module.exports = router;
+module.exports = router
