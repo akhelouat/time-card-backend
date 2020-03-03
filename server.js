5165da1efcc4706b0d0b64dca000aead86cb6c79
KHELOUAT Abdelmalek <ab.khelouat@gmail.com>
2020-03-03 02:19:32 +0100

    delete npm-debug.log and add it to .gitignore, fix cron bug in server.js

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
-/**
- * @ Author: Rahil Felix
- * @ Create Time: 2020-02-27 13:19:14
- * @ Modified by: Rahil Felix
- * @ Modified time: 2020-03-02 16:15:26
- * @ Description:
- */
 
 const http = require('http');
 const port = 3000;
 const router = require('./router')
 const server = http.createServer(router)
-var CronJob = require('cron').CronJob;
+var cron = require('node-cron')
 
-var job = new CronJob('00 00 08 * * *', function() {
-        // actualisation
-    }, function() {
-        /* cron stop funct */
-    },
-    true
-);
+cron.schedule('*/2 * * * *', () => {
+    // mettre la fonction ici
+  });
 
 server.listen(port)
 console.log('le server écoute sur le port ' + port)
