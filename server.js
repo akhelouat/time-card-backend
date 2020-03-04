c7a4992189f224cce2caccd588c8abe6bcbb0f79
KHELOUATAbdelMalek <ab.khelouat@gmail.com>
2020-03-04 10:28:42 +0100

    add axios and cron into server.js

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const http = require('http');
 const port = 3000;
 const router = require('./router')
 const server = http.createServer(router)
-var cron = require('node-cron')
+const cron = require('node-cron')
+const axios = require('axios')
 
-cron.schedule('*/2 * * * *', () => {
-    // mettre la fonction ici
-  });
+
+cron.schedule('0 1 * * *', () => {
+  console.log('fonction axios qui va se lancer automatiquement chaque matin')
+}, {
+  scheduled: true,
+  timezone: "Europe/Paris"
+});
 
 server.listen(port)
 console.log('le server écoute sur le port ' + port)
